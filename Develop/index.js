c3a86c7f419499cc2f9968e1e8e15dc77d582337
msimp <max.simpson@gmail.com>
2021-07-12 14:15:20 -0500

    yolo

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-// TODO: Create a function to initialize app
-function init() {}
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+// //     // Is the same as this...
+// //     profileDataArr.forEach(profileItem => console.log(profileItem))
+// //     ;
+// //   };
+  
+// //   printProfileData(profileDataArgs);
 
-// Function call to initialize app
-init();
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {};
+
+// // Function call to initialize app
+// init();
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+// console.log(generatePage());
+
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
