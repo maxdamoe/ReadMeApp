c8d376c2df9e2e204f35b36c35acd9c2f4bc3313
msimp <max.simpson@gmail.com>
2021-07-16 15:39:32 -0500

    recursion commit

--- Develop/index.js
+++ Develop/index.js
@@ -34,12 +34,17 @@ const promptUser = () => {
 
 
 
-const promptProject = () => {
+const promptProject = portfolioData => {
   console.log(`
 =================
 Add a New Project
 =================
 `);
+
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+
   return inquirer.prompt([
     {
       type: "input",
@@ -81,11 +86,22 @@ Add a New Project
       name: "confirmAddProject",
       message: "Would you like to enter another project?",
       default: false,
+    
     },
-  ]);
-};
+  ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+  };
+
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
