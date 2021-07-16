e705423cc692de991e039f067bd8113d404da05c
msimp <max.simpson@gmail.com>
2021-07-16 14:24:04 -0500

    Node.js familiarity, improving the use of inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,80 +1,91 @@
+const inquirer = require("inquirer");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
+// const [name, github] = profileDataArgs;
 
-// // const profileDataArgs = process.argv.slice(2, process.argv.length);
-// // console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-// // const printProfileData = profileDataArr => {
-// //     // This...
-// //     for (let i = 0; i < profileDataArr.length; i += 1) {
-// //       console.log(profileDataArr[i]);
-// //     }
-  
-// //     console.log('================');
-  
-// // //     // Is the same as this...
-// // //     profileDataArr.forEach(profileItem => console.log(profileItem))
-// // //     ;
-// // //   };
-  
-// // //   printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-// // // TODO: Include packages needed for this application
-
-// // // TODO: Create an array of questions for user input
-// // const questions = [];
-
-// // // TODO: Create a function to write README file
-// // function writeToFile(fileName, data) {}
-
-// // // TODO: Create a function to initialize app
-// // function init() {};
-
-// // // Function call to initialize app
-// // init();
-
-// // const generatePage = () => 'Name: Jane, Github: janehub';
-
-// // console.log(generatePage());
-
-// const inquirer = require('inquirer');
-
-// // const fs = require('fs');
-// // const [name, github] = profileDataArgs;
-
-// // const generatePage = require('./src/page-template');
-
-// // const pageHTML = generatePage(name, github);
-
-// // fs.writeFile('./index.html', pageHTML, err => {
-// //   if (err) throw err;
-
-// //   console.log('Portfolio complete! Check out index.html to see the output!');
-// // });
-
-
-// const promptUser = () => {
-//     return inquirer.prompt([
-//       {
-//         type: 'input',
-//         name: 'name',
-//         message: 'What is your name?'
-//       }
-//     ]);
-//   };
-  
-//   promptUser().then(answers => console.log(answers));
-
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
+  ]);
+};
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
